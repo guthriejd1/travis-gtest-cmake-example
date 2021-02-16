45d9c26886c7de9a60c56735dfb0a993759441be
guthriejd1 <guthriejd1@gmail.com>
2021-02-16 08:40:41 -0800

    Doing my best.

--- test/test_project1.cpp
+++ test/test_project1.cpp
@@ -10,9 +10,7 @@ TEST(IndependentMethod, ResetsToZero) {
 	independentMethod(i);
 	EXPECT_EQ(0, i);
 
-	i = 12;
-	independentMethod(i);
-	EXPECT_EQ(0,i);
+	EXPECT_EQ(0,0);
 }
 
 TEST(IndependentMethod, ResetsToZero2) {
